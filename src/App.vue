59f2de43884ade972f56f31cd27f5ee881b05a47
Josimar16 <josimarjr479@gmail.com>
2020-10-05 21:56:13 -0300

    biblioteca de terceiro instalada e projeto iniciado

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Organograma />
+  </div>
+</template>
+
+<script>
+import Organograma from "./components/Organograma.vue";
+
+export default {
+  name: "App",
+  components: {
+    Organograma,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
